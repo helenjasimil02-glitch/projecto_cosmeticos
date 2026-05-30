cf3b8175acf12b2a1c3214ad0622888a9d641951
helenjasimil02-glitch <helenjasimil02@gmail.com>
2026-05-30 21:45:44 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -89,4 +89,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'login'T_URL = '/'
 LOGOUT_REDIRECT_URL = 'login'
